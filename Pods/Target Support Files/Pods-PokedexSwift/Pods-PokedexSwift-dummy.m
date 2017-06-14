3a26a2906d8816ae2c7c0baece927b5f22d4b961
Ajisaputrars <ajisaputrars@gmail.com>
2017-06-14 12:00:37 +0700

    Fix the UI Problem in Detail Page

---
+++ Pods/Target Support Files/Pods-PokedexSwift/Pods-PokedexSwift-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PokedexSwift : NSObject
+@end
+@implementation PodsDummy_Pods_PokedexSwift
+@end
--- PokedexSwift/PokemonDetailVC.swift
+++ PokedexSwift/PokemonDetailVC.swift
@@ -10,14 +10,24 @@ import UIKit
 
 class PokemonDetailVC: UIViewController {
 
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var pokedexLbl: UILabel!
+    @IBOutlet weak var attackLbl: UILabel!
+    @IBOutlet weak var evoLbl: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    
     var pokemon: Pokemon!
-    @IBOutlet weak var labelTest: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        labelTest.text = pokemon.name
 
-        // Do any additional setup after loading the view.
     }
     @IBAction func backButtonPressed(_ sender: Any) {
         dismiss(animated: true, completion: nil)
