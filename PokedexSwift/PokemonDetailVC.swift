24e0b058c9e6c81dc8a835de44181a4bfead3fc6
Ajisaputrars <ajisaputrars@gmail.com>
2017-06-19 21:34:41 +0700

    Done. API discontinue starts 1 July 2017

--- PokedexSwift/PokemonDetailVC.swift
+++ PokedexSwift/PokemonDetailVC.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class PokemonDetailVC: UIViewController {
-
+    
     @IBOutlet weak var nameLbl: UILabel!
     @IBOutlet weak var mainImg: UIImageView!
     @IBOutlet weak var descriptionLbl: UILabel!
@@ -27,14 +27,41 @@ class PokemonDetailVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+        
+        nameLbl.text = pokemon.name.capitalized
+        
+        let img = UIImage(named: "\(pokemon.pokedexId)")
+        
+        mainImg.image = img
+        currentEvoImg.image = img
+        pokedexLbl.text = "\(pokemon.pokedexId)"
         pokemon.downloadPokemonDetail {
+            
+            // Whatever we write will only be called after the network call is compelte!
+            self.updateUI()
         }
-
     }
     
     func updateUI(){
+        attackLbl.text = pokemon.attack
+        defenseLbl.text = pokemon.defense
+        heightLbl.text = pokemon.height
+        weightLbl.text = pokemon.weight
+        typeLbl.text = pokemon.type
+        descriptionLbl.text = pokemon.description
         
+        if pokemon.nextEvolutionId == "" {
+            
+            evoLbl.text = "No Evolutions"
+            nextEvoImg.isHidden = true
+            
+        } else {
+            
+            nextEvoImg.isHidden = false
+            nextEvoImg.image = UIImage(named: pokemon.nextEvolutionId)
+            let str = "Next Evolution: \(pokemon.nextEvolutionName) - LVL \(pokemon.nextEvolutionLevel)"
+            evoLbl.text = str
+        }
     }
     
     
